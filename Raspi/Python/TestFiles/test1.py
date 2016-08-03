645e3c3a584e6cdea100ce4d0c95dbd455fd6dcb
juanortiz <Tiacw1bmff>
2016-08-03 15:52:18 -0400

    UPDATE

--- Raspi/Python/TestFiles/test1.py
+++ Raspi/Python/TestFiles/test1.py
@@ -7,5 +7,4 @@ arduino_address = 0x04
 
 while (True):
     time.sleep(.5)
-    nothing = [1]
-    print bus.read_i2c_block_data(arduino_address,0,nothing)
+    print bus.read_i2c_block_data(arduino_address,0)
