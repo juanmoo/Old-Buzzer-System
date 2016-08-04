c9fc65b60bb83ced9e7edec4f6cf6f42dc5e1617
juanortiz <Tiacw1bmff>
2016-08-04 18:24:51 -0400

    UPDATE

---
+++ Raspi/Python/TestFiles/LightUpdate.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+import time
+import smbus
+
+bus = smbus.SMBus(1)
+slave_address = 0x10
+
+onArr = []
+offArr = []
+
+for x in range(8):
+    onArr.append(1)
+    offArr.append(0)
+
+while True:
+    time.sleep(.5)
+    bus.write_i2c_block_data(slave_address,11,onArr)
+    time.sleep(.5)
+    bus.write_i2c_block_data(slave_address,11,offArr)
