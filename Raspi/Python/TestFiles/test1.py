37c9ed4da666f486c6e46437f788fb9ab3eac45d
juanortiz <Tiacw1bmff>
2016-08-04 18:10:40 -0400

    UPDATE

--- Raspi/Python/TestFiles/test1.py
+++ Raspi/Python/TestFiles/test1.py
@@ -9,4 +9,7 @@ while (True):
     time.sleep(.5)
     #bus.write_byte(arduino_address,0)
     data = bus.read_i2c_block_data(arduino_address,0)
+    data2 = []
+    for x in range(8):
+        data2.append(data[x])
     print data
