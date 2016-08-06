00bdf08747118cdd37bb3fd952a9e4c775034550
juanortiz <Tiacw1bmff>
2016-08-06 13:21:08 -0400

    UPDATE

--- Arduino/lib/Communicator/I2CLink.cpp
+++ Arduino/lib/Communicator/I2CLink.cpp
@@ -121,7 +121,7 @@ I2CLink::I2CLink()
 
         uint16_t totalTime_s = 60 * minutes + seconds;
         Serial.println("An attempt was made.");
-        Serial.println(totalTime_s);
+        Serial.print(totalTime_s);
         Serial.print(" seconds remain.\n");
         //user_updateTime(totalTime_s);
       }
